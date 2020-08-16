9d95e0bb587c265d0fa2365108e98c4f5c5e6b8a
Vitatiy <targetdeveloper57@gmail.com>
2020-08-16 20:12:42 +0300

    refractoring

--- js/common.js
+++ js/common.js
@@ -1,27 +1,35 @@
-const getStageItems = $('.selection-stages .selection-stages-item');
-const getNotification = $('.selection-stages .selection-stages-notification');
-const changeStage = (index) => {$('.selection-stages-wrapper').css('transform', `translate(-${index * 100}%, 0)`)};
-const toggleNotification = (id, elm, setMode = false) => {
-    let typeNotifacation = getNotification.find(`.selection-stages-notification__item[data-stage-id="${id}"]`).data('type-stage');
-    getNotification.find('.selection-stages-notification__item').slideUp();
-    setMode && getNotification.find(`.selection-stages-notification__item[data-stage-id="${id}"]`).slideDown();
-    setMode && typeNotifacation ? elm.addClass(typeNotifacation) : elm.removeClass(typeNotifacation);
-};
+function stagesBlock(nameBlock) {
+    const stagesContainer = $(`.selection-stages${nameBlock}`);
+    const getStageItems = stagesContainer.find('.selection-stages-item');
+    const getNotices = stagesContainer.find('.selection-stages-notices');
+    let typeNotices = ['success', 'error'].join(' ');
 
-getStageItems.find('.selection-stages-item-content').click(function () {
-    let activeClass = 'selection-stages-item--done',
-        item = $(this).parent(),
-        isDoneStage = item.hasClass(activeClass),
-        stagePart = item.parent(),
-        currentIndex = getStageItems.index(item) + 1;
+    const changeStage = (index) => { stagesContainer.find('.selection-stages-wrapper').css('transform', `translate(-${index * 100}%, 0)`) };
+    const toggleNotification = (id, elm, showNotice = false) => {
+        let notices = getNotices.children(),
+            typeNotice = notices.filter(`[data-stage-id="${id}"]`).data('type-stage');
+        notices.slideUp();
+        showNotice && notices.filter(`[data-stage-id="${id}"]`).slideDown();
+        showNotice && typeNotice ? elm.addClass(typeNotice) : elm.removeClass(typeNotice);
+    };
 
-    if (isDoneStage) {
-        getStageItems.slice(currentIndex - 1, getStageItems.length).removeClass(activeClass);
-        toggleNotification(currentIndex, item, false);
-        !item.prev().length && stagePart.prev().length && changeStage(stagePart.prev().index());
-    } else {
-        getStageItems.slice(0, currentIndex).addClass(activeClass);
-        toggleNotification(currentIndex, item, true);
-        !item.next().length && stagePart.next().length && changeStage(stagePart.next().index())
-    }
-});
+    getStageItems.find('.selection-stages-item-content').click(function () {
+        let activeClass = 'selection-stages-item--done',
+            item = $(this).parent(),
+            isDone = item.hasClass(activeClass),
+            stagePart = item.parent(),
+            currentIndex = getStageItems.index(item) + 1;
+
+        if (isDone) {
+            getStageItems.slice(currentIndex - 1, getStageItems.length).removeClass(`${activeClass} ${typeNotices}`);
+            toggleNotification(currentIndex, item);
+            !item.prev().length && stagePart.prev().length && changeStage(stagePart.prev().index());
+        } else {
+            getStageItems.slice(0, currentIndex).addClass(activeClass).removeClass(typeNotices);
+            toggleNotification(currentIndex, item, true);
+            !item.next().length && stagePart.next().length && changeStage(stagePart.next().index())
+        }
+    });
+}
+
+stagesBlock('.students');
