da3c205314f657cb1308bef5edbb04e491e4b599
Vitatiy <targetdeveloper57@gmail.com>
2020-08-18 11:21:43 +0300

    change structure, change func

--- js/common.js
+++ js/common.js
@@ -3,18 +3,19 @@ function stagesBlock(nameBlock) {
     const getStageItems = stagesContainer.find('.selection-stages-item');
     const getNotices = stagesContainer.find('.selection-stages-notices');
     const getPagination = stagesContainer.find('.selection-stages-pagination-item');
+    const postionsStage = [...getStageItems].map(item => item.offsetLeft);
     let typeNotices = ['success', 'error'].map(item => `selection-stages-item_status_${item}`).join(' ');
 
-    const changeStage = (index) => { 
-        stagesContainer.find('.selection-stages-wrapper').css('transform', `translate(-${index * 100}%, 0)`);
-        getPagination.removeClass('selection-stages-pagination-item--active');
-        getPagination.eq(index).addClass('selection-stages-pagination-item--active');
+    const changeStage = (index) => {
+        let stageEnd = getStageItems.index(getStageItems.filter(`[data-start-stage="${index}"]`));
+        stageEnd = stageEnd < 0 ? 0 : stageEnd;
+        stagesContainer.find('.selection-stages-wrapper').animate({ 'scrollLeft': postionsStage[stageEnd] }, 500);
     };
 
     const toggleNotification = (id, elm, showNotice = false) => {
         let notices = getNotices.children(),
             typeNotice = notices.filter(`[data-stage-id="${id}"]`).data('type-stage');
-            typeNotice = typeNotice ? `selection-stages-item_status_${typeNotice}` : '';
+        typeNotice = typeNotice ? `selection-stages-item_status_${typeNotice}` : '';
 
         notices.slideUp();
         showNotice && notices.filter(`[data-stage-id="${id}"]`).slideDown();
@@ -25,24 +26,24 @@ function stagesBlock(nameBlock) {
         let activeClass = 'selection-stages-item--done',
             item = $(this).parent(),
             isDone = item.hasClass(activeClass),
-            stagePart = item.parent(),
-            currentIndex = getStageItems.index(item) + 1;
+            currentIndex = item.index() + 1;
 
         if (isDone) {
             getStageItems.slice(currentIndex - 1, getStageItems.length).removeClass(`${activeClass} ${typeNotices}`);
             toggleNotification(currentIndex, item);
-            !item.prev().length && stagePart.prev().length && changeStage(stagePart.prev().index());
         } else {
             getStageItems.slice(0, currentIndex).addClass(activeClass).removeClass(typeNotices);
             toggleNotification(currentIndex, item, true);
-            !item.next().length && stagePart.next().length && changeStage(stagePart.next().index())
         }
         item.addClass('current');
-        getStageItems.not(`:eq(${currentIndex-1})`).removeClass('current');
-        
+        getStageItems.not(`:eq(${currentIndex - 1})`).removeClass('current');
     });
 
-    getPagination.click(function() {changeStage($(this).index())});
+    getPagination.click(function () {
+        changeStage($(this).index() + 1);
+        getPagination.removeClass('selection-stages-pagination-item--active');
+        $(this).addClass('selection-stages-pagination-item--active');
+    });
 }
 
 stagesBlock('.students');
