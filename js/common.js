b3f7f0df22341f293ac8f530c7d9dcc93e6f82a4
Vitatiy <targetdeveloper57@gmail.com>
2020-08-18 12:27:45 +0300

    scroll event

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 function stagesBlock(nameBlock) {
+    if (!$(`.selection-stages${nameBlock}`).length) return;
     const stagesContainer = $(`.selection-stages${nameBlock}`);
     const getStageItems = stagesContainer.find('.selection-stages-item');
     const getNotices = stagesContainer.find('.selection-stages-notices');
@@ -8,18 +9,18 @@ function stagesBlock(nameBlock) {
 
     const changeStage = (index) => {
         let stageEnd = getStageItems.index(getStageItems.filter(`[data-start-stage="${index}"]`));
-        stageEnd = stageEnd < 0 ? 0 : stageEnd;
+        stageEnd = stageEnd <= 0 ? 0 : stageEnd - 1;
         stagesContainer.find('.selection-stages-wrapper').animate({ 'scrollLeft': postionsStage[stageEnd] }, 500);
     };
 
-    const toggleNotification = (id, elm, showNotice = false) => {
+    const toggleNotification = (id, stage, showNotice = false) => {
         let notices = getNotices.children(),
             typeNotice = notices.filter(`[data-stage-id="${id}"]`).data('type-stage');
         typeNotice = typeNotice ? `selection-stages-item_status_${typeNotice}` : '';
 
         notices.slideUp();
         showNotice && notices.filter(`[data-stage-id="${id}"]`).slideDown();
-        showNotice && typeNotice ? elm.addClass(typeNotice) : elm.removeClass(typeNotice);
+        showNotice && typeNotice ? stage.addClass(typeNotice) : stage.removeClass(typeNotice);
     };
 
     getStageItems.find('.selection-stages-item-content').click(function () {
@@ -43,6 +44,20 @@ function stagesBlock(nameBlock) {
         changeStage($(this).index() + 1);
         getPagination.removeClass('selection-stages-pagination-item--active');
         $(this).addClass('selection-stages-pagination-item--active');
+    });
+
+    let position = 0;
+    
+    stagesContainer.bind('mousewheel', function (e) {
+        let maxWidth = stagesContainer.find('.selection-stages-wrapper')[0].scrollWidth;
+        if (e.originalEvent.wheelDelta / 120 > 0) {
+            position = maxWidth > position ? position += 20 : maxWidth.scrollWidth;
+        }
+        else {
+            position = position > 0 ? position -= 20 : 0;
+            console.log(position);
+        }
+        stagesContainer.find('.selection-stages-wrapper').scrollLeft(position);
     });
 }
 
